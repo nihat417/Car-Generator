b545d0ea8aa4733becfafc4f043616dd0cbcdebc
akrem <akrem@NIKO>
2023-03-08 19:02:05 +0400

    update 1.2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -23,44 +24,137 @@ namespace Car_Generator
     
     public partial class MainWindow : Window
     {
-        
+        CancellationTokenSource? cts = new();
+        Stopwatch? stopwatch = null;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void StartBtn_Click(object sender, RoutedEventArgs e)
+
+        public void SingleMode(CancellationToken cancellationTokenSource)
         {
+            var directory = new DirectoryInfo(@"..\..\..\Car Data");
+
+            new Thread(() =>
+            {
+                foreach (var item in directory.GetFiles())
+                {
+                    stopwatch = Stopwatch.StartNew();
+                    if (item.Extension == ".json")
+                    {
+                        var jsonText = File.ReadAllText(item.FullName);
+                        var listCar = JsonSerializer.Deserialize<List<Car>>(jsonText);
+
+                        if (listCar is null)
+                            continue;
+
+
+                        foreach (var car in listCar)
+                        {
+                           
+                            if (cancellationTokenSource.IsCancellationRequested)
+                            {
+                                stopwatch.Stop();
+                                Dispatcher.Invoke(() => timeSpan.Text = stopwatch.Elapsed.ToString());
+                                break;
+                            }
+
+                            Dispatcher.Invoke(() =>
+                            {
+                                Uc_Controls uC_Car = new(car);
+                                CarsGroupbox.Children.Add(uC_Car);
+                            });
+                            Dispatcher.Invoke(() => timeSpan.Text = stopwatch.Elapsed.ToString());
+                            Thread.Sleep(300);
+                        }
+                        stopwatch.Stop();
+
+                        Dispatcher.Invoke(() => timeSpan.Text = stopwatch.Elapsed.ToString());
+                    }
+                }
+            }).Start();
+            
+
+            
+        }
+
+
+        public void MultiMode(CancellationToken cancellationToken)
+        {
+
             var directory = new DirectoryInfo(@"..\..\..\Car Data");
 
             foreach (var item in directory.GetFiles())
             {
 
-                if (item.Extension == ".json")
+                ThreadPool.QueueUserWorkItem(state =>
                 {
-                    var jsonText = File.ReadAllText(item.FullName);
-                    var listCar = JsonSerializer.Deserialize<List<Car>>(jsonText);
-
-                    if (listCar is null)
-                        continue;
-
-                    foreach (var car in listCar)
+                    stopwatch = Stopwatch.StartNew();
+                    if (item.Extension == ".json")
                     {
+                        var jsonText = File.ReadAllText(item.FullName);
+                        var listCar = JsonSerializer.Deserialize<List<Car>>(jsonText);
+
+                        if (listCar is null)
+                            return;
 
 
+                        foreach (var car in listCar)
+                        {
 
-                        // lblTimeLoading.Text = watch.Elapsed.ToString();
-                        Uc_Controls uC_Car = new(car);
-                        CarsGroupbox.Children.Add(uC_Car);
-                        
+                            if (cancellationToken.IsCancellationRequested)
+                            {
+                                stopwatch.Stop();
+                                Dispatcher.Invoke(() => timeSpan.Text = stopwatch.Elapsed.ToString());
+                                break;
+                            }
+
+                            Dispatcher.Invoke(() =>
+                            {
+                                Uc_Controls uC_Car = new(car);
+                                CarsGroupbox.Children.Add(uC_Car);
+                            });
+                            Dispatcher.Invoke(() => timeSpan.Text = stopwatch.Elapsed.ToString());
+                            Thread.Sleep(300);
+                        }
+                        stopwatch.Stop();
+
+                        Dispatcher.Invoke(() => timeSpan.Text = stopwatch.Elapsed.ToString());
                     }
-                }
-
-
-
+                });
             }
-                
-            
+
+        }
+
+
+
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (Singlerdbtn.IsChecked == false && Multi.IsChecked == false)
+            {
+                MessageBox.Show("select 1 radio button");
+                return;
+            }
+            if (Singlerdbtn.IsChecked==true)
+            {
+                cts = new();
+                SingleMode(cts.Token);
+            }
+            if (Multi.IsChecked == true)
+            {
+                cts = new();
+                MultiMode(cts.Token);
+            }
+
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            cts?.Cancel();
+            timeSpan.Text = "00:00:00";
         }
     }
 }
